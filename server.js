1856a7a66d38477165b2b6fa479cb29a71360337
Alexei Rusu <rusualexei1995@gmail.com>
2022-11-07 21:54:38 -0500

    setting up the database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const drinks = require('./models/drinks')
 
 const app = express()
 
@@ -10,6 +11,9 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App!')
 })
 
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
+})
 
 app.listen(PORT, () => {
     console.log('Server listening on port ' + PORT)
