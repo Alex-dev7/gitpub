7c923add9448040fd9847f5318dc02904206eb29
Alexei Rusu <rusualexei1995@gmail.com>
2022-11-07 21:47:08 -0500

    setting up the server

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT 
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Gitpub App!')
+})
+
+
+app.listen(PORT, () => {
+    console.log('Server listening on port ' + PORT)
+})
