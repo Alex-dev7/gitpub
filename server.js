0258dce78a81bf4059fa3cb14b36b742ac16284e
Alexei Rusu <rusualexei1995@gmail.com>
2022-11-11 18:27:31 -0500

    food database/routes and homepage

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 require('dotenv').config()
 const express = require('express')
 const drinks = require('./models/drinks')
+const food = require('./models/food')
+
 
 const app = express()
 
+//middleware?
+app.use("/static", express.static("public"))
+
 const PORT = process.env.PORT 
 
-//creating home page route
+//creating home page route that will link drinks and food routes
 app.get('/', (req, res) => {
-    res.send('Welcome to the Gitpub App!')
+    res.render('home_index.ejs')
 })
 
+//----------DRINKS-------------------
 //creating a new route for the drinks
 app.get('/drinks', (req, res) => {
     res.render('drinks_index.ejs', {
@@ -22,6 +28,22 @@ app.get('/drinks', (req, res) => {
 app.get('/drinks/:id', (req, res) => {
     res.render('drinks_show.ejs', {
         drinks: drinks[req.params.id]
+    })
+})
+
+
+//----------FOOD-------------------
+//creating a new route for the food
+app.get('/food', (req, res) => {
+    res.render('food_index.ejs', {
+        food
+    })
+})
+
+//setting up show route for indiv food
+app.get('/food/:id', (req, res) => {
+    res.render('food_show.ejs', {
+        food: food[req.params.id]
     })
 })
 
